fcf90d697b9f2bd6e1319b5b86bc4ae84c0ce9fb
RJEduHuib <rjeduhuib@gmail.com>
2024-06-27 15:23:04 -0500

    Cambios

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -6,10 +6,10 @@ const indexCtl = {}
 indexCtl.mostrar = async (req, res) => {
     try {
         await sql.promise().execute('CREATE OR REPLACE VIEW clienteCompania AS SELECT c.*, d.*, p.* FROM clients c JOIN detailClients d ON d.clientIdClient = c.idClient JOIN pages p ON d.pageIdPage = p.idPage')
-        await sql.promise().execute('CREATE OR REPLACE VIEW permisosRol AS SELECT p.*, r.* FROM rolusers r JOIN permissions p ON p.rolUserIdRolUser = r.idRolUser')
-        await sql.promise().execute('CREATE OR REPLACE VIEW rolsPagina AS SELECT p.*, r.* FROM rolusers p JOIN rols r ON p.rolIdRol = r.idRol')
-        await sql.promise().execute('CREATE OR REPLACE VIEW usuarioPagina AS SELECT p.*, r.*,u.*,g.* FROM rolusers p JOIN rols r ON p.rolIdRol = r.idRol JOIN users u on p.userIdUser = u.idUser JOIN pages g ON p.pageIdPage = g.idPage')
-        await sql.promise().execute('CREATE OR REPLACE VIEW usuariosCompletos AS SELECT u.*, r.*, o.*, p.* FROM users u JOIN rolusers r JOIN rols o on r.rolIdRol = o.idRol JOIN permissions p ON p.rolUserIdRolUser = r.idRolUser');
+        await sql.promise().execute('CREATE OR REPLACE VIEW permisosRol AS SELECT p.*, r.* FROM rolUsers r JOIN permissions p ON p.rolUserIdRolUser = r.idRolUser')
+        await sql.promise().execute('CREATE OR REPLACE VIEW rolsPagina AS SELECT p.*, r.* FROM rolUsers p JOIN rols r ON p.rolIdRol = r.idRol')
+        await sql.promise().execute('CREATE OR REPLACE VIEW usuarioPagina AS SELECT p.*, r.*,u.*,g.* FROM rolUsers p JOIN rols r ON p.rolIdRol = r.idRol JOIN users u on p.userIdUser = u.idUser JOIN pages g ON p.pageIdPage = g.idPage')
+        await sql.promise().execute('CREATE OR REPLACE VIEW usuariosCompletos AS SELECT u.*, r.*, o.*, p.* FROM users u JOIN rolUsers r JOIN rols o on r.rolIdRol = o.idRol JOIN permissions p ON p.rolUserIdRolUser = r.idRolUser');
         await sql.promise().execute('CREATE OR REPLACE VIEW clienteComprador AS SELECT c.*, s.*, d.* FROM clients c JOIN detailClients d ON d.clientIdClient = c.idClient JOIN sells s ON d.sellIdSell = s.idSell')
         res.render('login/index', { csrfToken: req.csrfToken() });
     } catch (error) {
