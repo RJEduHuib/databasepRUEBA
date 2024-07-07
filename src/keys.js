79c76f23f0efea982328b85ac0ef08bf7677ef5d
RJEduHuib <rjeduhuib@gmail.com>
2024-07-07 09:25:28 -0500

    cambios cpanel

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 // Configuración de variables de entorno para la conexión a la base de datos MySQL
-const MYSQLHOST = process.env.MYSQLHOST ?? 'bv5ubtbegiqlkmgpwmsf-mysql.services.clever-cloud.com'; // Host de la base de datos
-const MYSQLUSER = process.env.MYSQLUSER ?? 'u0fu6cwfcydtkgx0'; // Usuario de la base de datos
-const MYSQLPASSWORD = process.env.MYSQLPASSWORD ?? 'qZK75Y9U5HCncC2Y65Ak'; // Contraseña de la base de datos
-const MYSQLDATABASE = process.env.MYSQLDATABASE ?? 'bv5ubtbegiqlkmgpwmsf'; // Nombre de la base de datos
+const MYSQLHOST = process.env.MYSQLHOST ?? '65.21.94.181'; // Host de la base de datos
+const MYSQLUSER = process.env.MYSQLUSER ?? 'ditelcom_database'; // Usuario de la base de datos
+const MYSQLPASSWORD = process.env.MYSQLPASSWORD ?? 'Database@15121982.'; // Contraseña de la base de datos
+const MYSQLDATABASE = process.env.MYSQLDATABASE ?? 'ditelcom_solutiontec'; // Nombre de la base de datos
 const MYSQLPORT = process.env.MYSQLPORT ?? '3306'; // Puerto de la base de datos
-const MYSQL_URI = process.env.MYSQL_URI ?? 'mysql://u0fu6cwfcydtkgx0:qZK75Y9U5HCncC2Y65Ak@bv5ubtbegiqlkmgpwmsf-mysql.services.clever-cloud.com:3306/bv5ubtbegiqlkmgpwmsf'; // URI de conexión a la base de datos (si es necesario)
+const MYSQL_URI = process.env.MYSQL_URI ?? ''; // URI de conexión a la base de datos (si es necesario)
 
 // Exportar las variables de configuración
 module.exports = {
